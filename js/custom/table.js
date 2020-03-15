7e4e7f07e422af05153b86448177bf63c1a62e3e
amittal151 <abhimittal151@gmail.com>
2020-03-16 02:14:18 +0530

    c

--- js/custom/table.js
+++ js/custom/table.js
@@ -80,14 +80,14 @@ function sortTable(n,numb) {
 function myFunction() {
   // Declare variables 
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
+  input = document.getElementById("myInput1");
   filter = input.value.toUpperCase();
-  table = document.getElementById("tb1");
+  table = document.getElementsByTagName("tb1");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsById("td")[0];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
