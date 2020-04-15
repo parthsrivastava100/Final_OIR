5800420d296b0a098c3c0b3f9328356a8e508417
parthsrivastava100 <58399016+parthsrivastava100@users.noreply.github.com>
2020-04-15 09:48:39 +0530

    fixed bugs

---
+++ main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el:'#vue_use',
+data:{
+contact:'Contact/contact_us.html',
+fa:'FAQ.pdf',
+mo:'Mou/Mou.html',
+apply:'howToApply.html',
+iitk:'http://iitk.ac.in',
+faculty:'Faculty/faculty.html',
+is:'International students/int-Students.html',
+iitks:'IITK Students/iitkstudents.html',
+atiitk:'At IITK/atiitk.html',
+oir:'https://www.iitk.ac.in/oir/',
+appo:''
+}
+});
